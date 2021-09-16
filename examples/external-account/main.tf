2c049532c73099e73cc2ff834fcdef5fddfa6750
Muhammad Fadhilah Sukmojatmiko <t-fadhilah@traveloka.com>
2021-09-16 10:47:51 +0700

    Upgrade terraform-aws-iam-role module to terraform v0.13 compatible

--- examples/external-account/main.tf
+++ examples/external-account/main.tf
@@ -4,17 +4,17 @@ provider "aws" {
 
 module "this" {
   # In actual use case, you have to replace the following line (line 8) with:
-  # source = "github.com/traveloka/terraform-aws-iam-role.git//modules/external?ref=v0.6.0"
+  #source = "github.com/traveloka/terraform-aws-iam-role.git//modules/external?ref=v2.0.2"
   source = "../../modules/external"
 
-  role_name                  = "ThirdPartyName" # Replace this value with the name given by 3rd party, or define it by yourself with clear and describing name
+  role_name                  = "ThirdPartyName2" # Replace this value with the name given by 3rd party, or define it by yourself with clear and describing name
   role_path                  = "/external/security/"
   role_description           = "Role for ThirdParty"
   role_force_detach_policies = "true"
   role_max_session_duration  = "43200"
 
-  account_id  = "arn:aws:sts::460124681500:assumed-role/CloudInfraIntern/t-fadhilah@traveloka.com" # Replace this value 
-  external_id = "arn:aws:sts::460124681500:assumed-role/CloudInfraIntern/t-fadhilah@traveloka.com" # Replace this value
+  account_id  = "123321" # Replace this value 
+  external_id = "456654" # Replace this value
 
   product_domain = "tst"
   environment    = "production"
